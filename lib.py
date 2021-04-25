1631cc49db67dca060451ef06593e58fd2e1b782
hansglick <hansglick@gmail.com>
2021-04-25 23:03:57 +0200

    pacific polygon

--- lib.py
+++ lib.py
@@ -5,16 +5,21 @@ import folium
 import py3dep
 import json
 import shapely
+import pickle 
 
 from shapely.strtree import STRtree
 from shapely.geometry import shape
 from shapely.geometry import Polygon
 from shapely.geometry import MultiPolygon
 from shapely.geometry import Point
+from os import listdir
 
 import geopandas as gpd
 import pandas as pd
 import numpy as np
+
+
+
 
 from scipy import stats
 from folium import plugins
